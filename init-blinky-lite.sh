58a3a47e52e13c60c0ef59637c36eb8bd90ff3e4
David McGinnis <david.mcginnis@thequantumguys.se>
2018-10-05 07:43:08 +0200

    made local automatic startup

---
+++ init-blinky-lite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo su - pi -c "/usr/bin/screen -S ui -d -m /home/pi/blinky-lite/run-blinky-lite-ui.sh"
+
---
+++ run-blinky-lite-ui.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 1
+cd /home/pi/blinky-lite/blinky-lite-ui
+source runNodeRed.sh
+
+
