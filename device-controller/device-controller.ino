4504bc2ceae10a5cc8124aa2589dc867cd1a786e
David McGinnis <david.mcginnis@thequantumguys.se>
2018-10-23 16:37:48 +0200

    added interupt routine

--- device-controller/device-controller.ino
+++ device-controller/device-controller.ino
@@ -1,8 +1,4 @@
 #include "device-controller.h"
-// comment
-int loopCount = 0;
-int loopCountMax = 1000;
-float deltaMicros = 0;
 unsigned long nowTime;
 unsigned long loopStartTime;
 unsigned long startTime1Hz;
@@ -21,7 +17,10 @@ int overIPin = 18;
 int overILeakPin = 17;
 
 int qTop = 0;
+int qTopSet = 0;
 int qEnable = 0;
+int qTopSign = 1;
+int qTopRampDelay = 1;
 float vAcOutMeas;
 float vAcOutMeasAvg = 0;
 float iAcOutMeas;
@@ -32,27 +31,25 @@ float tempMeas2;
 float tempMeas2Avg = 0;
 float tempMeas3;
 float tempMeas3Avg = 0;
-float powerAvg = 0;
 boolean blinky = false;
 boolean led1 = false;
 boolean led2 = false;
 boolean led3 = false;
-int overI = 0;
-int overIOld = 0;
-int overILeak = 0;
-int overILeakOld = 0;
-boolean overITrip = false;
-boolean overILeakTrip = false;
+volatile boolean  overIInterrupt = false;
+boolean  overI = false;
+float nsamples = 6100.0;
+float nsampleRate;
 
 void setup()
 {
-  setupCommunications(true, 19200);
+  setupCommunications(true, 9600);
 //  Serial.begin(9600);
   delay(200);
   
-  nowTime = micros();
+  nowTime = millis();
   loopStartTime = nowTime;
   startTime1Hz = nowTime;
+  nsampleRate = 1.0 / nsamples;
 
   pinMode(qTopPin, OUTPUT);     
   pinMode(qEnablePin, OUTPUT);     
@@ -64,6 +61,7 @@ void setup()
   pinMode(led1Pin, OUTPUT);     
   pinMode(led2Pin, OUTPUT);     
   pinMode(led3Pin, OUTPUT);     
+  pinMode(13, OUTPUT);     
   pinMode(overIPin, INPUT);     
   pinMode(overILeakPin, INPUT);     
   digitalWrite(qEnablePin, qEnable);    
@@ -94,108 +92,83 @@ void setup()
   pinMode(26, OUTPUT);     
   digitalWrite(26, LOW);     
 
-
-
-
-  delay(1000);
-  printMessage("qEnable", intToString(qEnable));
-  printMessage("qTop", intToString(qTop));
-  printMessage("overITrip", booleanToString(overITrip));
-  printMessage("overILeakTrip", booleanToString(overILeakTrip));
+  attachInterrupt(overIPin, overIRising, RISING);
+  delay(200);
  
 }
 
 void loop()
 {
   if (dataOnSerial())
-  {
-/*    
-    Serial.print("Topic: ");
-    Serial.print(getInputTopic());
-    Serial.print(" Payload: ");
-    Serial.println(getInputPayload());
-*/   
-    
+  {    
     if (getInputTopic().equals("setQEnable"))
     {
+      qTop = 0;
+      analogWrite(qTopPin,qTop);
+      printMessage("qTop", intToString(qTop));
       qEnable = stringToInt(getInputPayload());
       digitalWrite(qEnablePin, qEnable);
       printMessage("qEnable", intToString(qEnable));
       if (qEnable > 0)
       {
-        overITrip = false;
-        printMessage("overITrip", booleanToString(overITrip));
-        overIOld = 0;
-        overILeakTrip = false;
-        printMessage("overILeakTrip", booleanToString(overILeakTrip));
-        overILeakOld = 0;
+        overI = false;
+        printMessage("overI", booleanToString(overI));
       }
     }
     if (getInputTopic().equals("getQEnable")) printMessage("qEnable", intToString(qEnable));
     
     if (getInputTopic().equals("setQTop"))
     {
-      qTop = stringToInt(getInputPayload());
-      analogWrite(qTopPin,qTop);
+      qTopSet = stringToInt(getInputPayload());
+      qTopSign = 1;
+      if (qTopSet < qTop) qTopSign = -1;
+      while (qTopSet != qTop)
+      {
+        qTop = qTop + qTopSign;
+        delay(qTopRampDelay);
+        analogWrite(qTopPin,qTop);
+      }
       printMessage("qTop", intToString(qTop));
     }
     if (getInputTopic().equals("getQTop")) printMessage("qTop", intToString(qTop));
+    
+    if (getInputTopic().equals("setQTopRampDelay")) qTopRampDelay = stringToInt(getInputPayload());
+    if (getInputTopic().equals("getQTopRampDelay")) printMessage("qTopRampDelay", intToString(qTopRampDelay));
 
-    if (getInputTopic().equals("getOverITrip")) printMessage("overITrip", booleanToString(overITrip));
-    if (getInputTopic().equals("getOverILeakTrip")) printMessage("overILeakTrip", booleanToString(overILeakTrip));
+    if (getInputTopic().equals("setNsamples"))
+    {
+      nsamples = stringToFloat(getInputPayload());
+      nsampleRate = 1.0 / nsamples;
+    }
+    if (getInputTopic().equals("getNsamples")) printMessage("nsamples", floatToString(nsamples, 2));
+    
+    if (getInputTopic().equals("getOverI")) printMessage("overI", booleanToString(overI));
   }
 
   vAcOutMeas = (float) analogRead(vAcOutMeasPin);
-  vAcOutMeasAvg = vAcOutMeasAvg + (vAcOutMeas - vAcOutMeasAvg) * deltaMicros * 0.000001;
+  vAcOutMeasAvg = vAcOutMeasAvg + (vAcOutMeas - vAcOutMeasAvg) * nsampleRate;
   iAcOutMeas = (float) analogRead(iAcOutMeasPin);
-  iAcOutMeasAvg = iAcOutMeasAvg + (iAcOutMeas - iAcOutMeasAvg) * deltaMicros * 0.000001;
+  iAcOutMeasAvg = iAcOutMeasAvg + (iAcOutMeas - iAcOutMeasAvg) * nsampleRate;
   tempMeas1 = (float) analogRead(tempMeas1Pin);
-  tempMeas1Avg = tempMeas1Avg + (tempMeas1 - tempMeas1Avg) * deltaMicros * 0.000001;
+  tempMeas1Avg = tempMeas1Avg + (tempMeas1 - tempMeas1Avg) * nsampleRate;
   tempMeas2 = (float) analogRead(tempMeas2Pin);
-  tempMeas2Avg = tempMeas2Avg + (tempMeas2 - tempMeas2Avg) * deltaMicros * 0.000001;
+  tempMeas2Avg = tempMeas2Avg + (tempMeas2 - tempMeas2Avg) * nsampleRate;
   tempMeas3 = (float) analogRead(tempMeas3Pin);
-  tempMeas3Avg = tempMeas3Avg + (tempMeas3 - tempMeas3Avg) * deltaMicros * 0.000001;
-  powerAvg = powerAvg + ((vAcOutMeas * iAcOutMeas) - powerAvg) * deltaMicros * 0.000001;
+  tempMeas3Avg = tempMeas3Avg + (tempMeas3 - tempMeas3Avg) * nsampleRate;
 
-  overI = digitalRead(overIPin);
-  overILeak = digitalRead(overILeakPin);
-
-  if ((overI == 1) && (overIOld == 1) )
+  if (overIInterrupt)
   {
     qEnable = 0;
-    digitalWrite(qEnablePin, qEnable);
     qTop = 0;
-    analogWrite(qTopPin,qTop);
+    overI = true;
     printMessage("qEnable", intToString(qEnable));
     printMessage("qTop", intToString(qTop));
-    overITrip = true;
-    printMessage("overITrip", booleanToString(overITrip));
+    printMessage("overI", booleanToString(overI));
+    overIInterrupt = false;
   }
-  overIOld = overI;
-/*
-  if ((overILeak == 1) && (overILeakOld == 1) )
-  {
-    qEnable = 0;
-    digitalWrite(qEnablePin, qEnable);
-    qTop = 0;
-    analogWrite(qTopPin,qTop);
-    printMessage("qEnable", intToString(qEnable));
-    printMessage("qTop", intToString(qTop));
-    overILeakTrip = true;
-    printMessage("overILeakTrip", booleanToString(overILeakTrip));
-  }
-  overILeakOld = overILeak;
-*/  
-  ++loopCount;
-  nowTime = micros();
-  if (loopCount > loopCountMax)
-  {     
-      deltaMicros = (float)(nowTime - loopStartTime) / ((float)loopCountMax);
-      loopCount = 0;
-      loopStartTime = nowTime;
-  }
+  nowTime = millis();
 
-  if ((nowTime - startTime1Hz) > 1000000)
+  if ((nowTime - startTime1Hz) > 1000)
   {
     startTime1Hz = nowTime;
     blinky = !blinky;
@@ -205,13 +178,21 @@ void loop()
     digitalWrite(led1Pin, led1);
     digitalWrite(led2Pin, led2);
     digitalWrite(led3Pin, led3);
+    digitalWrite(13, blinky);
     printMessage("vAcOutMeasAvg", floatToString(vAcOutMeasAvg, 2));
-//    printMessage("iAcOutMeasAvg", floatToString(iAcOutMeasAvg, 2));
+    printMessage("iAcOutMeasAvg", floatToString(iAcOutMeasAvg, 2));
     printMessage("tempMeas1Avg", floatToString(tempMeas1Avg, 2));
-//    printMessage("tempMeas2Avg", floatToString(tempMeas2Avg, 2));
-//    printMessage("tempMeas3Avg", floatToString(tempMeas3Avg, 2));
-//    printMessage("powerAvg", floatToString(powerAvg, 2));
-    printMessage("loopTime", floatToString(deltaMicros,2));
+    printMessage("tempMeas2Avg", floatToString(tempMeas2Avg, 2));
+    printMessage("tempMeas3Avg", floatToString(tempMeas3Avg, 2));
     printMessage("blinky", booleanToString(blinky));
   }
 }
+void overIRising() 
+{
+  digitalWrite(qEnablePin, LOW);
+  analogWrite(qTopPin,0);
+  overIInterrupt = true;
+}
+
+
+
